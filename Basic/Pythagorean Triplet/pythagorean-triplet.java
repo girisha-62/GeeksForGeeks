dcc85abc9aab4e70a025d37b31b70f2286a944a2
girisha-62 <100276969+girisha-62@users.noreply.github.com>
2023-01-28 17:45:48 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Pythagorean Triplet/pythagorean-triplet.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            String[] inputLine = br.readLine().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            boolean ans = new Solution().checkTriplet(arr, n);
+            System.out.println(ans ? "Yes" : "No");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    boolean checkTriplet(int[] arr, int n) {
+        // code here
+        for(int i=0;i<n;i++){
+            arr[i]=arr[i]*arr[i];
+        }
+        Arrays.sort(arr);
+        for (int i = n - 1; i >= 2; i--) {
+            int l = 0;
+            int r = i - 1;
+            while (l < r) {
+                if (arr[l] + arr[r] == arr[i])
+                    return true;
+                if (arr[l] + arr[r] < arr[i])
+                    l++;
+                else
+                    r--;
+            }
+        }
+        return false;
+    }
+}
