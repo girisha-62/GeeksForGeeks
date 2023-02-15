5fdc02683497f9cada94e43225a9a88be404d8d1
girisha-62 <100276969+girisha-62@users.noreply.github.com>
2023-02-15 21:38:46 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Swap and Maximize/swap-and-maximize.java
@@ -0,0 +1,97 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+
+
+class GFG
+{
+    long maxSum(long arr[] ,int n)
+    {
+     if(n==1) return arr[n-1];
+
+        
+
+        long sum =0;
+
+        Arrays.sort(arr);
+
+        int start =0;
+
+        int last =n-1;
+
+        while(start<last) {
+            sum+=(arr[last]-arr[start]);
+
+            if(start+1!=last) {
+
+                sum+=(arr[last]-arr[start+1]);
+
+            }
+
+            start++;
+
+            last--;
+
+        }
+
+        
+
+        sum+=arr[start]-arr[0];
+
+        return sum;
+        
+    }
+}
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+
+    // Driver code
+    public static void main (String[] args) throws IOException{
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while(testcases-- > 0){
+            int n=Integer.parseInt(br.readLine());
+//            String line = br.readLine();
+//            String[] a2 = line.trim().split("\\s+");
+//            int n =Integer.parseInt(a2[0]);
+//            int k =Integer.parseInt(a2[1]);
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            long arr[]=new long[n];
+            for(int  i=0;i<n;i++)
+            {
+                arr[i]=Long.parseLong(a1[i]);
+            }
+            GFG ob=new GFG();
+            //ArrayList<Long> ans=ob.smallestDifferenceTriplet(a,b,c,n);
+            long ans=ob.maxSum(arr,n);
+            System.out.println(ans);
+        }
+    }
+}
+
+
+
+
+// } Driver Code Ends
