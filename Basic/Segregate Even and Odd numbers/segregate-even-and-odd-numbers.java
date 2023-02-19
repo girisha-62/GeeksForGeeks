05d65c2ae56c9072e88ef7a779fd48e4abf47d84
girisha-62 <100276969+girisha-62@users.noreply.github.com>
2023-02-19 19:35:04 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Segregate Even and Odd numbers/segregate-even-and-odd-numbers.java
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().segregateEvenOdd(arr, n);
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+
+class Solution {
+
+    void segregateEvenOdd(int arr[], int n) {
+
+         Arrays.sort(arr);
+
+        ArrayList<Integer> ale= new ArrayList<>();
+
+        for(int i =0; i<n; i++){
+
+             if(arr[i]%2==0){
+
+                 ale.add(arr[i]);
+
+             }
+
+        }
+
+        for(int i=0;i<n; i++){
+
+            if(arr[i]%2!=0){
+
+                ale.add(arr[i]);
+
+            }
+
+        }
+
+        for(int i=0; i<n; i++){
+
+            arr[i]=ale.get(i);
+
+        }
+
+    }
+
+}
