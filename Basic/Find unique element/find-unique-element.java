a83ca99efb5853e69447bde63f72063d47e1b9b9
girisha-62 <100276969+girisha-62@users.noreply.github.com>
2023-02-25 23:06:27 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Find unique element/find-unique-element.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+            
+            int n = Integer.parseInt(stt.nextToken());
+            int k = Integer.parseInt(stt.nextToken());
+            
+            int a[] = new int[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            System.out.println(obj.findUnique(a, n, k));
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    
+    public int findUnique(int a[], int n, int k)
+    {
+        Arrays.sort(a);
+        int result = a[n - 1];
+        for (int i = 0;  i < n; i+=k) {
+            if ( (i + k - 1) < n && a[i] != a[i + k - 1]) {
+                result = a[i];
+                break;
+            }
+        }
+        return result;
+    }
+}
