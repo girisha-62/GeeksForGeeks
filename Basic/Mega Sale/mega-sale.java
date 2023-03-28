479fbf7d70636eeede1e4ea699ec3a1b3e05117e
girisha-62 <100276969+girisha-62@users.noreply.github.com>
2023-03-28 22:31:57 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Mega Sale/mega-sale.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*; 
+class GFG{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            
+            String[] xy = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(xy[0]);
+            int m = Integer.parseInt(xy[1]);
+            
+            
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) 
+                a[i] = Integer.parseInt(str[i]);
+            
+            Compute g = new Compute();
+            System.out.println(g.maxProfit(a, n, m));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Compute {
+
+    public static long maxProfit(int a[], int n, int m) {
+        // Your code goes here 
+       Arrays.sort(a);
+
+        long profit = 0;
+
+        for(int i=0; i<Math.min(m,n) && a[i]<0; i++)
+
+        {
+
+            profit += a[i];
+
+        }
+
+        return -profit;
+    }
+}
