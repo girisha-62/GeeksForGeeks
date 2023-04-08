db53194043e779d3bd519a742d233fbc33c92eb2
girisha-62 <100276969+girisha-62@users.noreply.github.com>
2023-04-08 22:47:23 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Minimize sum of alternate product/minimize-sum-of-alternate-product.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            Compute obj = new Compute();
+            System.out.println(obj.altProduct(a, n));
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Compute {
+    
+    public long altProduct(long arr[], long n)
+    {
+        // Your code goes here
+        Arrays.sort(arr);
+        long s=0;
+        int i=0;
+        int j=(int)n-1;
+        while(i<j){
+            s+=arr[i]*arr[j];
+            i++;
+            j--;
+        }
+        return s;
+    }
+}
