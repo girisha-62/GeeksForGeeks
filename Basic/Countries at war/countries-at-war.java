49059dfdf3319f74d923521077eac659b122c002
girisha-62 <100276969+girisha-62@users.noreply.github.com>
2023-04-11 23:25:51 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Countries at war/countries-at-war.java
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		     
+		    int arr [] = new int[sizeOfArray];
+		    int brr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    line = br.readLine();
+		    String[] ele = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        brr[i] = Integer.parseInt(ele[i]);
+		    }
+		    Complete obj = new Complete();
+		    String ans = obj.Country_at_war(arr, brr, sizeOfArray);
+		    System.out.println(ans);
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static String Country_at_war (int arr[], int brr[], int n) {
+        //Complete the function
+        int A=0;
+
+        int B=0;
+
+        for(int i=0;i<n;i++){
+
+            if(arr[i]>brr[i])
+
+            A++;
+
+            if(brr[i]>arr[i])
+
+            B++;
+
+        }
+
+        if(A>B)
+
+        return "A";
+
+        else if(B>A)
+
+               return "B";
+
+               else return "DRAW";
+    }
+    
+    
+}
+
+
