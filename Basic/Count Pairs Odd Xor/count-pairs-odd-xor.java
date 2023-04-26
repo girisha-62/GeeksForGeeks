b8bcfaedc8b62799a5e97b0172eb8bdcac06bb98
girisha-62 <100276969+girisha-62@users.noreply.github.com>
2023-04-26 18:24:10 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Count Pairs Odd Xor/count-pairs-odd-xor.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+class Solution
+{
+    long countXorPair(long arr[], int n)
+    {
+ long countEven=0;
+        int countOdd=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]%2==0)
+              countEven++;
+            else
+                countOdd++;
+        }
+        return countEven*countOdd;
+    }
+}
+
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+
+    // Driver code
+    public static void main(String[] args) throws IOException {
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while (testcases-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            long a[] = new long[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(a1[i]);
+            }
+            Solution ob = new Solution();
+            //ArrayList<Long> ans=ob.smallestDifferenceTriplet(a,b,c,n);
+            long ans = ob.countXorPair(a,n);
+            System.out.println(ans);
+        }
+    }
+}
+
+
+
+
+
+// } Driver Code Ends
